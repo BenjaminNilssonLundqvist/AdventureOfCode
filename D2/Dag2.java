1ca0967c76027a5883c8ecc8c1a44fd69ba45cb7
BenjaminNilssonLundqvist <benjamin.n.lundqvist@gmail.com>
2022-12-01 21:11:38 +0100

    D1

---
+++ D2/Dag2.java
@@ -0,0 +1,12 @@
+package D2;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Dag2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan= new Scanner(new File("D1/ADOC1.txt"));
+    }
+    
+}
